45bb38a40d14fbfbd16d6a41c9ac2f420f506682
Lhdinh2306 <dinh.lieuhy2004@hcmut.edu.vn>
2024-06-27 18:22:42 +0700

    initial

---
+++ myduyen.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    window.scrollTo(0, 0);
+    alert("Page is loaded");
+    var newContent = document.getElementById("home").innerHTML;
+    document.getElementById("total").innerHTML = newContent;
+}
+function HM() {
+    window.scrollTo(0, 0);
+    var newContent = document.getElementById("home").innerHTML;
+    document.getElementById("total").innerHTML = newContent;
+}
+function CD() {
+    window.scrollTo(0, 0);
+    var newContent = document.getElementById("CD").innerHTML;
+    document.getElementById("total").innerHTML = newContent;
+}
+function HT() {
+    window.scrollTo(0, 0);
+    var newContent = document.getElementById("HT").innerHTML;
+    document.getElementById("total").innerHTML = newContent;
+}
+function SK() {
+    window.scrollTo(0, 0);
+    var newContent = document.getElementById("SK").innerHTML;
+    document.getElementById("total").innerHTML = newContent;
+}
+function DK() {
+    window.scrollTo(0, 0);
+    var newContent = document.getElementById("DK").innerHTML;
+    document.getElementById("total").innerHTML = newContent;
+}
+function CT() {
+    window.scrollTo(0, 0);
+    var newContent = document.getElementById("contact").innerHTML;
+    document.getElementById("total").innerHTML = newContent;
+}
